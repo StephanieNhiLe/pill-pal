41d58e93d560ade530bc6257936ac43da7aa4713
Vishal Pugazhendhi <vishal.pugazhendhi@gmail.com>
2024-09-29 13:04:31 -0700

    bug fixes

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,23 @@
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
-from .services.PineconeVectorDB import PineconeVectorDB
-from .services.PrescriptionProcessing import PrescriptionUploader
+from services.PineconeVectorDB import PineconeVectorDB
+from services.PrescriptionProcessing import PrescriptionUploader
 
 app = FastAPI()
 
+OPENAI_API_KEY = "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"
+PINECONE_API_KEY = "235a04ec-e6c1-41cb-b830-507a96da48a8"
+PINECONE_INDEX_NAME = "prescription-index"
+
 # Create a PineconeVectorDB instance
-pinecone_db = PineconeVectorDB("235a04ec-e6c1-41cb-b830-507a96da48a8", "Open-api-key-here", "prescription-index")
-# Create an index/initialize the Pinecone index
-pinecone_db.create_index()
+pinecone_db = PineconeVectorDB(PINECONE_API_KEY, OPENAI_API_KEY, PINECONE_INDEX_NAME)
+
+# Check if the index exists
+if not pinecone_db.check_index_exists():
+    # Create an index/initialize the Pinecone index
+    pinecone_db.create_index()
+
 # Get the vector store
 uploader = PrescriptionUploader(pinecone_db)
 
--- backend/services/PineconeVectorDB.py
+++ backend/services/PineconeVectorDB.py
@@ -1,9 +1,11 @@
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
+import os
 
 class PineconeVectorDB:
     def __init__(self, pinecone_api_key, openai_api_key, index_name):
+        os.environ['PINECONE_API_KEY'] = pinecone_api_key
         self.pc = Pinecone(api_key=pinecone_api_key)
         self.index_name = index_name
         self.embeddings = OpenAIEmbeddings(api_key=openai_api_key)
@@ -14,8 +16,16 @@ class PineconeVectorDB:
                 name=self.index_name,
                 dimension=1536,
                 metric="cosine",
-                spec=ServerlessSpec(cloud="aws", region="us-west-2")
+                spec=ServerlessSpec(cloud="aws", region="us-east-1")
             )
+    
+    def check_index_exists(self):
+        try:
+            self.pc.describe_index(self.index_name)
+            print(f"Index {self.index_name} exists")
+            return True
+        except Exception:
+            return False
     
     def get_vector_store(self):
         return PineconeVectorStore(index_name=self.index_name, embedding=self.embeddings)
